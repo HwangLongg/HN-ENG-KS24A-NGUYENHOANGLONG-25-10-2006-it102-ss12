e200589cee66a018135979d931e82e6690070875
HwangLongg <longlongokok2@gmail.com>
2024-12-12 00:35:45 +0700

    Update session-12-ex-05.c

--- session-12-ex-05.c
+++ session-12-ex-05.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
-int main (){
-    int n ,m ;
-    printf ("moi nhap vao 2 so nguyen to ");
-    scanf ("%d",&n);
-    scanf ("%d",&m);
-    if ( n >= 0){
-        printf ("%d la so nguyen to\n",n);
+
+int kiemTraSoNguyenTo(int n) {
+    if (n <= 1) {
+        return 0;
     }
-    else {
-        printf ("%d khong phai so nguyen to\n ",n);
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
     }
-    if (m >= 0){
-         printf ("%d la so nguyen to\n ",m);
+    return 1;
+}
+
+int main() {
+    int n, m;
+
+    printf("Mời nhập vào 2 số nguyên: ");
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    if (kiemTraSoNguyenTo(n)) {
+        printf("%d là số nguyên tố\n", n);
+    } else {
+        printf("%d không phải số nguyên tố\n", n);
     }
-    else {
-        printf ("%d khong phai so nguyen to\n ",m);
+
+    if (kiemTraSoNguyenTo(m)) {
+        printf("%d là số nguyên tố\n", m);
+    } else {
+        printf("%d không phải số nguyên tố\n", m);
     }
+
     return 0;
 }
