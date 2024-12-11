e7956fb7951dc5b378de099b1a78ab0bab0f9ec2
HwangLongg <longlongokok2@gmail.com>
2024-12-12 00:30:33 +0700

    Update session-12-ex-04.c

--- session-12-ex-04.c
+++ session-12-ex-04.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-int main (){
-    int n , max  ;
-    printf ("moi nhap vao so luong phan tu");
-    scanf ("%d",&n);
-    int arr[n];
-    for ( int i = 0;i<n;i++){
-        scanf ("%d",&arr[i]);
-    }
-    for (int i = 0;i <n ; i++){
-        if ( max < arr[i]){
-            max = arr[i] ;
-        }
 
+int timMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
     }
-    printf ("so lon nhat la %d ",max);
+    return max;
+}
+
+int main() {
+    int n;
+
+    printf("Mời nhập vào số lượng phần tử: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Mời nhập vào các phần tử:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = timMax(arr, n);
+    printf("Số lớn nhất là: %d\n", max);
+
     return 0;
 }
