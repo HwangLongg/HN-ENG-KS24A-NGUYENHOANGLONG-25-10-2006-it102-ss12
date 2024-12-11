7658f5db233dcdffd3e8f1937d8509c3488b54f5
HwangLongg <longlongokok2@gmail.com>
2024-12-12 00:26:35 +0700

    Update session-12-ex-01.c

--- session-12-ex-01.c
+++ session-12-ex-01.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-int main (){
-    int sum , n , m ;
-    printf ("moi nhap vao 2 so bat ky ");
-    scanf ("%d",&n);
-    scanf ("%d",&m);
-    sum = n + m ;
-    printf ("tong 2 so dem duoc la : %d ",sum);
+
+int tinhTong(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int n, m, sum;
+    printf("Mời nhập vào 2 số bất kỳ: ");
+    scanf("%d", &n);
+    scanf("%d", &m);
+    
+    sum = tinhTong(n, m); 
+
+    printf("Tổng 2 số được nhập là: %d\n", sum);
+
     return 0;
 }
