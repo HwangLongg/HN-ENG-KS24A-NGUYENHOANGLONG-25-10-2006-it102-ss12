5347922bbba4263188a3ea77776ea3b485f55b9d
HwangLongg <longlongokok2@gmail.com>
2024-12-12 00:28:12 +0700

    Update session-12-ex-02.c

--- session-12-ex-02.c
+++ session-12-ex-02.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
-int main (){
-int n ,m ;
-printf ("moi nhap vao so luong phan tu");
-scanf ("%d",&m);
-int arr[m];
-printf ("moi nhap vao tung phan tu");
-for (int i = 0;i<m;i++){
-    scanf ("%d",&arr[i]);
-    printf ("%d",arr[i]);
+
+int nhapMang(int arr[], int m) {
+    printf("Mời nhập vào từng phần tử của mảng:\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &arr[i]);
+    }
+    return 0;
 }
+
+int inMang(int arr[], int m) {
+    printf("Mảng bạn vừa nhập là:\n");
+    for (int i = 0; i < m; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int main() {
+    int m;
+
+    printf("Mời nhập vào số lượng phần tử trong mảng: ");
+    scanf("%d", &m);
+
+    int arr[m];
+
+    nhapMang(arr, m);
+    inMang(arr, m);
+
+    return 0;
 }
